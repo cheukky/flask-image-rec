4f0904db0638823d169117a9d6dde10b610de242
ctt43 <ctt43@cornell.edu>
2018-09-05 16:13:01 -0400

    initialize the 4 pages of app, no routing yet

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # flask-image-rec
 This goal of this project is to create a web app that allows users to upload pictures of Gundam faces and get a reasonably accurate prediction back
+
+## Index
+Allows user to upload picture of subject
+
+## Result
+Gives user the prediction and allows user to confirm or deny it
+
+## Training
+Gives user the chance to apply correct label to image, which is then saved to database and used in subsequent training sets
+
+## Thanks
+Allows user to start again
+
+
+Run Flask app:
+```shell
+export FLASK_APP=app.py
+flask run
+```
+
+To Stop:
+Press CTRL+C
+
+Run Dockerfile:
+```shell
+docker build -t cheukky/flaskapp .
+docker run --rm -p 8888:5000 --name flaskapp cheukky/flaskapp
+```
+
+To Stop:
+```shell
+docker stop flaskapp
+```
--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # list of cat images
 images = [
-   "http://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26388-1381844103-11.gif",
+    "http://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26388-1381844103-11.gif",
     "http://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr01/15/9/anigif_enhanced-buzz-31540-1381844535-8.gif",
     "http://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26390-1381844163-18.gif",
     "http://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr06/15/10/anigif_enhanced-buzz-1376-1381846217-0.gif",
@@ -19,10 +19,30 @@ images = [
     "http://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr03/15/10/anigif_enhanced-buzz-11980-1381846269-1.gif"
     ]
 
+
 @app.route('/')
 def index():
     url = random.choice(images)
     return render_template('index.html', url=url)
+
+
+@app.route('/result')
+def result():
+    url = "https://vignette.wikia.nocookie.net/gundam/images/4/43/Gundam_Barbatos_standing_over_Sandoval_Rueters_fallen_suit.png/revision/latest/scale-to-width-down/600?cb=20161024131944"
+    return render_template('result.html', url=url)
+
+
+@app.route('/thanks')
+def thanks():
+    url = "https://vignette.wikia.nocookie.net/gundam/images/a/a7/636723.jpg/revision/latest/scale-to-width-down/600?cb=20141018031606"
+    return render_template('thanks.html', url=url)
+
+
+@app.route('/training')
+def training():
+    url = "https://vignette.wikia.nocookie.net/gundam/images/9/93/00QanTSwordBitsBeam.jpg/revision/latest/scale-to-width-down/600?cb=20120921123309"
+    return render_template('training.html', url=url)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
